c8aecebcfdacce9b29827d6a4a5e55af4826a594
Brian Ortega <brian.ortega@gmail.com>
2012-05-24 21:53:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plymlet::Application.config.secret_token = '9bb96c8fbdaccedb7a7833988f4bf979639029a07857d769f920eab2b7a0916b79be2ffd5343bc18050e13553770e35dd1365377a317cdc89065b6fc87ea621a'
