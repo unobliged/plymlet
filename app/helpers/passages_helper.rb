2862eb232cb63e6a3d3f3200c2cd33f189484395
Brian Ortega <brian.ortega@gmail.com>
2012-05-28 03:56:10 +0200

    Put li tags in view instead of passages helper

--- app/helpers/passages_helper.rb
+++ app/helpers/passages_helper.rb
@@ -2,7 +2,6 @@ module PassagesHelper
 
   def unique_words(content)
     @words = content.downcase.gsub(/[[:punct:]]/,"").split(' ').uniq.sort
-    @words.map { |word| content_tag(:li, word) }
   end
 
   def definition_for_word
