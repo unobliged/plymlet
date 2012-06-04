fc5bf03e7deb40504bd7ad45fe8a93377ec602cb
Brian Ortega <brian.ortega@gmail.com>
2012-06-04 04:44:39 +0200

    Adds cedict define_word method to passages_helper using custom script

--- app/helpers/passages_helper.rb
+++ app/helpers/passages_helper.rb
@@ -1,16 +1,26 @@
 module PassagesHelper
-require 'wordnet'
-require 'zidian'
   def unique_words(content)
     @words = content.downcase.gsub(/[[:punct:]]/,"").split(' ').uniq.sort
   end
 
   def define_word(word)
+    @cedict = 'cedict.txt'
+    @word_matches = File.readlines(@cedict).grep(/#{word}/)
+
+    @match = []
+
+    @word_matches.each do |x|
+      @match = x.split
+      #return x if @match[0].include?(word) or @match[1].include?(word)
+      return x if @match[0]==(word) or @match[1]==(word)
+    end
+
+# Old code from testing zidian and wordnet
 #    @cnword = Zidian.find(word)   
-    lemmas = WordNet::WordNetDB.find(word)
-    synsets = lemmas.map { |lemma| lemma.synsets }
-    words = synsets.flatten
-    words.each { |word| puts word.gloss }
+#    lemmas = WordNet::WordNetDB.find(word)
+#    synsets = lemmas.map { |lemma| lemma.synsets }
+#    words = synsets.flatten
+#    words.each { |word| puts word.gloss }
   end
 
 end
---
+++ ex
@@ -0,0 +1,16 @@
+module PassagesHelper
+#require 'wordnet'
+#require 'zidian'
+  def unique_words(content)
+    @words = content.downcase.gsub(/[[:punct:]]/,"").split(' ').uniq.sort
+  end
+
+  def define_word(word)
+#    @cnword = Zidian.find(word)   
+    lemmas = WordNet::WordNetDB.find(word)
+    synsets = lemmas.map { |lemma| lemma.synsets }
+    words = synsets.flatten
+    words.each { |word| puts word.gloss }
+  end
+
+end
